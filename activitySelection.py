242ae121cb85ce2eb86898bfebe042a49b173f39
Lucy Zhang <lucy.zhang3@duke.edu>
2017-01-25 11:56:52 -0500

    finished activity selection

--- activitySelection.py
+++ activitySelection.py
@@ -2,6 +2,7 @@
 You are given n activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
 '''
 from sort import fullMergeSort
+
 start  =  [1, 3, 0, 5, 8, 5];
 finish =  [2, 4, 6, 7, 9, 9];
 
@@ -9,13 +10,13 @@ def sort(start, finish):
 	activities = []
 	sortFinish = fullMergeSort(finish)
 	prevFinish = None
-	for i, elem in sortFinish:
-		if prev is None:
+	for i in range(len(sortFinish)):
+		if prevFinish is None:
 			activities.append(i)
-			prevFinish = elem
+			prevFinish = sortFinish[i]
 		elif start[i]>prevFinish:
 			activities.append(i)
-			prevFinish = elem
+			prevFinish = sortFinish[i]
 
 	return activities
 
