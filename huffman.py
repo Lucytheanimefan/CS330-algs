45db9be236a698fd936e36618742c22c02022150
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-01-29 04:17:59 -0500

    Started huffman coding

---
+++ huffman.py
@@ -0,0 +1,22 @@
+arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+freq = [5, 9, 12, 13, 16, 45];
+
+class Node:
+	def __init__(self, letter, freq):
+		self.letter = letter
+		self.freq = freq
+		self.left = None
+		self.right = None
+
+	def __str__(self): 
+		return letter + ": "+freq
+
+
+
+class HuffmanTree:
+	def __init__(self):
+		self.root = None
+
+	def insert(node):
+		
+
